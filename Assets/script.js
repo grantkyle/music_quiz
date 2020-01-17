98cfb32d1148d4cfaccff02d5f92ac2e2daec10d
Grant Kyle <sgrantkyle@gmail.com>
2020-01-16 19:47:17 -0600

    almost final push

--- Assets/script.js
+++ Assets/script.js
@@ -17,7 +17,7 @@ $nextButton.addEventListener("click", () => {
 var $questionDiv = document.getElementById('questions');
 var $answersDiv = document.getElementById('answers');
 var $resultsDiv = document.getElementById('results')
-
+var $highScoresDiv = document.getElementById('highscores')
 
 
 var timeEl = document.querySelector(".time");
@@ -26,13 +26,10 @@ var mainEl = document.getElementById("main");
 var questionIndex = 0;
 var secondsLeft = 75;
 var numCorrect = 0;
-//questionDiv.textContent = myQuestions[questionIndex++].question;
-//answersDiv.innerHTML = myQuestions[questionIndex].answers.map(answers => {
-//return (`${<input type='radio' name="question" value={answers}>${answers}</input>}`)
-//})
+
 // timer set to count down from 75
 
-//initialize timer variable at global scope
+//this is to reset test
 var timer = ''
 function resetQuiz() {
     questionIndex = 0;
@@ -142,128 +139,39 @@ function endGame() {
     //check to store high scores
 }
 
-/////////******** create function to prompt user for initials, those save to local storage
 //////// stringify then parse is option
 /////// then appendChild to front end
+
 function saveScore() {
+    var userInitials = prompt("Enter Name Here");
 
-    localStorage.setItem(initials, numCorrect)
+    var userObj = {
+        name: userInitials,
+        score: numCorrect
+    };
+
+    var userInfo = localStorage.getItem("userInfo");
+
+    var userArr;
+    if (!userInfo) {
+        userArr = [];
+    } else {
+        userArr = JSON.parse(userInfo);
+    }
+
+    userArr.push(userObj);
+    localStorage.setItem("userInfo", JSON.stringify(userArr));
+
+
+    for (var end = userArr.length - 1, userCount = 1; end > -1; end-- , userCount) {
+
+        if (userArr.length === 5) { break; }
+        $highScoresDiv.innerHTML = (userArr[end].score + " - " + userArr.name);
+    }
+
+    // localStorage.setItem(userInitials, numCorrect)
+    // $highScoresDiv.innerHTML = (numCorrect + " - " + userInitials)
+    // only save highest 10 scores
+    // store all scores in the array
+    // display 10 scores 
 }
-
-
-
-// answerButtonsElement.addEventListener("click", function (event) {
-//     var itemClicked = event.target;
-
-//     if (itemClicked.matches("button")) {
-//         if (itemClicked.getAttribute("data-answer") === "false") {
-//             // subtract timer 15-    
-//             alert("wrong");
-//         }
-
-//         currentQuestionIndex++;
-//         setNextQuestion();
-
-//     }
-
-// });
-// // if time runs out, send message to start over
-// // i need to add function to reset page
-
-
-// var quizContainer = document.getElementById('quiz');
-// var resultsContainer = document.getElementById('results');
-// var submitButton = document.getElementById('submit');
-
-// generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-// function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
-//     function showQuestions(questions, quizContainer) {
-//         // code to show output 
-//         var output = [];
-//         var answers;
-//         // code to create a for loop to loop through all the questions
-
-//         for (var i = 0; i < questions.length; i++) {
-//             // make sure answers are reset when quiz starts
-//             answers = [];
-
-//             // make sure that an html button is added to each possible answer choice
-
-//             for (letter in questions[i].answers) {
-
-//                 // html radio button
-//                 // honestly, what is happening in this code below? 
-//                 // i understand holistically that it is creating buttons for the choices 
-//                 // but what are all the moving parts doing?
-
-//                 answers.push(
-//                     '<label>'
-//                     + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-//                     + letter + ': '
-//                     + questions[i].answers[letter]
-//                     + '</label>');
-//             }
-
-//             // add current question and its choices to the output
-//             output.push(
-//                 '<div class="question">' + questions[i].question + '</div>'
-//                 + '<div class="answers">' + answers.join('') + '</div>'
-//             );
-//         }
-
-//         quizContainer.innerHTML = output.join('');
-//     }
-
-//     function showResults(questions, quizContainer, resultsContainer) {
-//         // code to collect answer containers from the quiz
-
-//         var answerContainers = quizContainer.querySelectorAll('.answers');
-
-//         // track user response
-//         var userAnswer = '';
-
-//         // specify to start count from 0
-//         var numCorrect = 0;
-
-//         // create a for loop for each question
-
-//         for (var i = 0; i < questions.length; i++) {
-
-//             // and find selected choice...  what is happening in below code
-//             userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-//             // if answer is correct
-//             if (userAnswer === questions[i].correctAnswer) {
-//                 // add to the total number of questions answered correctly
-//                 // the two plus signs at the end of numCorrect
-//                 // ensure the number of correct responses keeps aggregating
-
-//                 numCorrect++;
-
-//                 // manipulate DOM to color correct answer green
-//                 answerContainers[i].style.color = 'lightgreen';
-//             }
-
-//             // else below is for when answer is incorrect or blank
-
-//             else {
-//                 answerContainers[i].style.color = 'red';
-//             }
-//         }
-//         // display aggregated number of correct answers out of total
-//         resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-//     }
-
-//     // display questions 
-
-//     showQuestions(questions, quizContainer);
-
-//     // when user clicks submit, show results
-//     submitButton.onclick = function () {
-//         showResults(questions, quizContainer, resultsContainer);
-//         console.log('startButton')
-//     }
-// }
-// // can "choices" be changed into curly brackets
